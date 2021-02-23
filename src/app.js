b4284808fc62f16bcae75b66f91bdc876537414f
Lucas Maceda <lucasmacedamiranda@gmail.com>
2021-02-22 23:31:31 -0300

    arquivos do desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const cors    = require("cors");
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,85 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories); // lista todos repositórios
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs } = request.body;
+  
+  // cria um objeto
+  repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  // insere o objeto dentro do array de repositórios
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  // realiza busca do repositório no array pelo id
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  // caso não seja encontrado
+  if(repositoryIndex < 0){
+    return response.status(400).send();
+  }
+
+  // caso seja encontrado
+  const repository = { 
+    id, 
+    title, 
+    url, 
+    techs, 
+    likes: repositories[repositoryIndex].likes, };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  // realiza busca do repositório no array pelo id
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  // caso não seja encontrado
+  if(repositoryIndex < 0){
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  // caso seja encontrado
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  // realiza busca do repositório no array pelo id
+  const repository = repositories.find(repository => repository.id === id);
+
+  // caso não seja encontrado
+  if(!repository){
+    return response.status(400).send();
+  }
+
+  // caso seja encontrado
+  repository.likes++;
+
+  return response.json(repository);
 });
 
 module.exports = app;
+
