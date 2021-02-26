866ef606611ad7af16b96aafd24993df1e3c3a23
Lucas Maceda <lucasmacedamiranda@gmail.com>
2021-02-25 22:06:13 -0300

    correção de bugs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+  /** 
+   * Métodos HTTP:
+   * 
+   * GET: Buscar informações do back-end
+   * POST: Criar uma informação no back-end
+   * PUT/PATCH: Alterar uma informação no back-end
+   * DELETE: Deletar uma informação no back-end
+   *
+   * *
+   * Tipos de parâmetros:
+   * 
+   * Query Params:   Filtros e paginação
+   * Route Params:   Identificar recursos na hora de atualizar ou deletar 
+   * Request Params: 
+   */
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
